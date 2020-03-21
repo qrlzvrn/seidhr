be025a503724141fb9dddde5ebb64c56307dbc78
qrlzvrn <qrlzvrn@yandex.ru>
2020-03-21 16:52:41 +0300

    Дополнен пакет db
    	-- Добавленный функционал:
    		-Добавление пользователя
    		-Проверка существования пользователя
    		-Заполнение таблицы с лекарствами из текстового файла
    		-Проверка существования лекарства в базе данных

--- db/db.go
+++ db/db.go
@@ -8,7 +8,8 @@ import (
 	"github.com/qrlzvrn/seidhr/config"
 )
 
-func ConnectToBD() (*sqlx.DB, error) {
+// ConnectToDB ...
+func ConnectToDB() (*sqlx.DB, error) {
 	dbConf, err := config.NewDBConf()
 	if err != nil {
 		return nil, err
@@ -20,4 +21,51 @@ func ConnectToBD() (*sqlx.DB, error) {
 	} else {
 		return db, nil
 	}
+}
+
+// CreateUser ...
+func CreateUser(db *sqlx.DB, tguserID int, chatID int) error {
+	tx := db.MustBegin()
+	tx.MustExec("INSERT INTO tguser (id, chat_id) VALUES ($1, $2)", tguserID, chatID)
+	err := tx.Commit()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// CheckUser ...
+func CheckUser(db *sqlx.DB, tguserID int) (bool, error) {
+	var isExist bool
+	err := db.QueryRow("SELECT exists (select 1 from tguser where id=$1)", tguserID).Scan(&isExist)
+	if err != nil {
+		return false, err
+	}
+	return isExist, nil
+}
+
+// InitMedList ...
+func InitMedList(db sqlx.DB, medLines []string) error {
+	tx := db.MustBegin()
+
+	for _, med := range medLines {
+		tx.MustExec("INSERT INTO medicament (title) VALUES ($1)", med)
+	}
+	err := tx.Commit()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// CheckMed ...
+func CheckMed(db sqlx.DB, medName string) (bool, error) {
+
+	var isExist bool
+	err := db.QueryRow("SELECT exists (select 1 from medicament where title=$1)", medName).Scan(&isExist)
+	if err != nil {
+		return false, err
+	}
+	return isExist, nil
 }
