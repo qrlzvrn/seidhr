d7157836d167c345f27d02af4151eea4ae12213c
qrlzvrn <qrlzvrn@yandex.ru>
2020-03-22 15:26:06 +0300

    В пакете db реализованы функции подписок и операций с ними
    	--Подписаться
    	--Отписаться
    	--Вывести все подписки
    	--Проверить наличие хотя бы одной подписки у пользователя

--- db/db.go
+++ db/db.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
@@ -64,6 +65,60 @@ func CheckMed(db sqlx.DB, medName string) (bool, error) {
 
 	var isExist bool
 	err := db.QueryRow("SELECT exists (select 1 from medicament where title=$1)", medName).Scan(&isExist)
+	if err != nil {
+		return false, err
+	}
+	return isExist, nil
+}
+
+// Subscribe - создает новую подписку пользователя на лекарство
+func Subscribe(db sqlx.DB, tguserID int, medicamentID int) error {
+	tx := db.MustBegin()
+	tx.MustExec("INSERT INTO subscription (tguser_id, medicament_id) VALUES ($1, $2)", tguserID, medicamentID)
+	err := tx.Commit()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Unsubscribe - отменяет у пользьователя подписку на лекарство
+func Unsubscribe(db *sqlx.DB, tguserID int, medicamentID int) error {
+	tx := db.MustBegin()
+	tx.MustExec("DELETE FROM subscription WHERE tguser_id = $1 AND medicament_id = $2", tguserID, medicamentID)
+	err := tx.Commit()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// ListSubscriptions - находит все подписки пользователя и возвращает [][]string, где
+// [[id title] [id title] [id title]]
+func ListSubscriptions(db *sqlx.DB, tguserID int) ([][]string, error) {
+	rows, err := db.Query("SELECT id, title from medicament INNER JOIN subscription on medicament.id=subscription.medicament_id WHERE subscription.tguser_id = $1", tguserID)
+	if err != nil {
+		return nil, err
+	}
+
+	subscriptions := [][]string{}
+
+	for rows.Next() {
+		var id int
+		var title string
+
+		rows.Scan(&id, &title)
+		subscriptions = append(subscriptions, []string{strconv.Itoa(id), title})
+		defer rows.Close()
+	}
+	return subscriptions, nil
+}
+
+// CheckSubscriptions - проверяет наличие у пользователя подписок на лекарства и,
+// если у него есть хоть одна подписка, возвращает true.
+func CheckSubscriptions(db *sqlx.DB, tguserID int) (bool, error) {
+	var isExist bool
+	err := db.QueryRow("SELECT exists (SELECT 1 FROM subscription WHERE tguser_id=$1)", tguserID).Scan(&isExist)
 	if err != nil {
 		return false, err
 	}
