6b2080d53edff95aa4b56e88aa0dfb00fdf09c2a
qrlzvrn <qrlzvrn@yandex.ru>
2020-04-18 17:45:34 +0300

    Изменен путь для config.toml

--- config/config.go
+++ config/config.go
@@ -35,7 +35,7 @@ type SSL struct {
 func InitConf() (Config, error) {
 	var conf Config
 
-	if _, err := toml.DecodeFile("./config.toml", &conf); err != nil {
+	if _, err := toml.DecodeFile("/config.toml", &conf); err != nil {
 		return conf, err
 	}
 	return conf, nil
